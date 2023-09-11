2b241528541b35e29064675174e6dcdfe4fdb924
M. Sevón <89909972+msevon@users.noreply.github.com>
2023-09-11 19:51:17 +0300

    Update config.py

--- config/config.py
+++ config/config.py
@@ -1,1 +1,13 @@
+let config = {};
 
+const test = Deno.env.get('TEST_ENVIRONMENT')
+
+if (test) {
+  //TEST DATABASE
+  config.database = Deno.env.get('DATABASE_URL');
+} else {
+  //REAL DATABASE
+  config.database = Deno.env.get('DATABASE_URL');
+}
+
+export { config };
